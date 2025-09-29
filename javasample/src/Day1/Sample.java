1ae69e89a9e01ecc2cb34f355ede728e9ce722c2
LENOVO <LENOVO@DESKTOP-KOO8GEL>
2025-09-29 19:55:43 +0530

    this is a basic java program

--- javasample/src/Day1/Sample.java
+++ javasample/src/Day1/Sample.java
@@ -2,9 +2,7 @@ package Day1;
 
 public class Sample {
 	public static void main(String[]args) {
-		System.out.println("welcome to the git");
-		
-		
+		System.out.println("welcome to the git");	
 	}
 
 }
