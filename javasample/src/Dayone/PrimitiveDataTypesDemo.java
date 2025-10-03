46319ddcafe556ff342aac313e6381241cc42ef9
LENOVO <LENOVO@DESKTOP-KOO8GEL>
2025-10-03 20:14:54 +0530

    this is day1 - day3 work

---
+++ javasample/src/Dayone/PrimitiveDataTypesDemo.java
@@ -0,0 +1,29 @@
+package Dayone;
+
+public class PrimitiveDataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int - 4bytes
+		int maxInt = 2147483647;
+		int minInt = -2147483648;
+		System.out.println("\nMinint range of byte is" +
+				minInt+"\nMaxint range of byte is "+maxInt);
+		
+		//long - 8bytes
+		long maxLong = 9223372036854775807L;
+		long minLong = -9223372036854775808L;
+		
+		System.out.println("\nMinlong range of byte is" +
+				minLong+"\nMaxlong range of byte is "+maxLong);
+		
+		float f=3234.141243278345f;
+		double d=3456.14124512345678902345678914f;
+		System.out.println("\nfloat value is "+f+" \ndouble value is "+d);
+		
+		//boolean 
+		boolean flag=false;
+		System.out.println("\nboolean value is "+flag);
+	}
+
+}
---
+++ javasample/src/core_javaprogram/day2.java
@@ -0,0 +1,13 @@
+package core_javaprogram;
+
+public class day2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main method");
+		main(20);
+		}
+     public static void main(int a) {
+    	 System.out.print("overloaded main method"+ a);
+	}
+}
