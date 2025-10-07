15639a9308fb9c150af46c305267006d5e9d18c5
LENOVO <LENOVO@DESKTOP-KOO8GEL>
2025-10-07 11:23:33 +0530

    this is Day1-Day3 work

---
+++ javasample/src/com/tnsif/daythree/encapsulation/Encapsulation.java
@@ -0,0 +1,16 @@
+package com.tnsif.daythree.encapsulation;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//object creation 
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
