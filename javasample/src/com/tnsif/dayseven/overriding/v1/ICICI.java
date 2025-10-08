d08a107b2e29f05eefe99a694cfcfb89836bc16f
LENOVO <LENOVO@DESKTOP-KOO8GEL>
2025-10-08 22:09:10 +0530

    this is Day7 work

---
+++ javasample/src/com/tnsif/dayseven/overriding/v1/ICICI.java
@@ -0,0 +1,12 @@
+
+//Program to demonstrate method overriding - Runtime Polymorphism
+package com.tnsif.dayseven.overriding.v1;
+
+//subclass
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
