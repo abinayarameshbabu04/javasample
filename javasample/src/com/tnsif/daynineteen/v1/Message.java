ecae571900de9a243818f7158c39f39967e45286
LENOVO <LENOVO@DESKTOP-KOO8GEL>
2025-10-26 22:24:20 +0530

    Day11-Day14 & Day19

---
+++ javasample/src/com/tnsif/daynineteen/v1/Message.java
@@ -0,0 +1,7 @@
+
+package com.tnsif.daynineteen.v1;
+
+@FunctionalInterface
+public interface Message{  
+    public void greet(String name);    
+ } 
---
+++ javasample/src/com/tnsif/daynineteen/v1/Palindrome.java
@@ -0,0 +1,7 @@
+
+
+package com.tnsif.daynineteen.v1;
+
+public interface Palindrome<T> {
+	boolean checkPalindrome(T data);
+}
